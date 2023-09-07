354b33778cc811b1a9a141595b56927b5402e9b2
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-07 12:22:30 -0700

    add 5-printf.c script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - Print a line with using printf functions
- * Return: 0 (Success)
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function
+ * Return: always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
