164960865098cb42bd556626bfe68a304ccb8e5a
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-07 09:59:57 -0700

    add 5-printf script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-
+#include  <stdio.h>
+/**
+ * main - Print aline with using a printf functions
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
