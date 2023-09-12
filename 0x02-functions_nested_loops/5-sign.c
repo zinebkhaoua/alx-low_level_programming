8c1c5440073d2d0560aa1e8a5c062cb1f8dc2e46
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-12 13:43:46 -0700

    add 6-abs.c script

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ *print_sign-print+ifngeaterthanzero 
+ *0 if n is zero
+ *and - if n less than zero
+ *@n: takes integer type input for function
+ *Return: 1 if + ,0 if 0, -1 if -
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs.c - function that computes asolute value of an integer
+ *@n: takes in integer type input for function
+ *Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
