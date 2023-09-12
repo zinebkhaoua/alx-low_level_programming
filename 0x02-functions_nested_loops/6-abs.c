9740ecce04a6767130312b83086fe67e0c7d7f83
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-12 14:15:19 -0700

    add 6-abs.c script

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *_abs.c - function that computes the absolute value of an integer
+ *@n: integer input
+ *Return: The absolute value of the integer
  */
-int main(void)
+int _abs(int n)
 {
-int r;
-
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+if (n < 0)
+n = -n;
+return (n);
 }
