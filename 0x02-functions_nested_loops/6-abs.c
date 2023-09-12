85a13392e65c950f0e9c9de10aff44d195e3b967
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-12 13:55:28 -0700

    add 6-abs.c script

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- *_abs.c - function that computes asolute value of an integer
- *@n: takes in integer type input for function
- *Return: Always 0 (Success)
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-int _abs(int n)
+int main(void)
 {
-if (n < 0)
-n = (-1) * n;
-return (n);
+int r;
+
+r = _abs(-1);
+printf("%d\n", r);
+r = _abs(0);
+printf("%d\n", r);
+r = _abs(1);
+printf("%d\n", r);
+r = _abs(-98);
+printf("%d\n", r);
+return (0);
 }
