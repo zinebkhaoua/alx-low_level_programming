ea0cf10c5f796bce6cd08b50c8fc58be097fccc7
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-12 09:12:04 -0700

    add 1-alphabet.c script

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+
+_putchar('\n');
+}
