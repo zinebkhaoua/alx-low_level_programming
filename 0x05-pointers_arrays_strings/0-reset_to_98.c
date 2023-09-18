f3fc062078aac259b7d3555d0502b23f0019d392
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-18 07:18:25 -0700

    add 0-reset_to_98.c script

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ *reset_to_98 - a functions that takes a pointer to an int a parameter
+ *and updates the value it points to to 98.
+ *Return: n
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+my readme file about 0x05-pointers_arrays_strings
