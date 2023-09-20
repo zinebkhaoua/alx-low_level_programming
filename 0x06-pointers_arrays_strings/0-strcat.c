e10815d880c9df9917da6d695483fed56d2a3abe
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-20 11:52:24 -0700

    add 0-strcat.c script

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strcat - Concatenates two strings.
+ *@dest: The destination string.
+ *@src: The source string.
+ *
+ *Return: A pointer to the resulting string `dest`.
+ */
+char *_strcat(char *dest, char *src)
+{
+int dest_len = 0, i = 0;
+
+while (dest[dest_len] != '\0')
+dest_len++;
+
+while (src[i] != '\0')
+{
+dest[dest_len] = src[i];
+dest_len++;
+i++;
+}
+
+dest[dest_len] = '\0';
+return (dest);
+}
