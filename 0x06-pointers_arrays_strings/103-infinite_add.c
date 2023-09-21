421f643c20fb41b1198cfd8bc2afeb4cef2d1a4b
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-21 15:10:14 -0700

    add 103-infinite_add.c script

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * infinite_add - Adds two numbers represented as strings
+ * @n1: The first number as a string
+ * @n2: The second number as a string
+ * @r: The buffer to store the result
+ * @size_r: The size of the buffer
+ *
+ * Return: A pointer to the result, or 0 if the result cannot be stored in r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int carry = 0;
+int i = 0, j = 0, k = 0;
+
+while (n1[i] != '\0' || n2[j] != '\0' || carry)
+{
+int digit1 = (n1[i] != '\0') ? (n1[i] - '0') : 0;
+int digit2 = (n2[j] != '\0') ? (n2[j] - '0') : 0;
+int sum = digit1 + digit2 + carry;
+
+carry = sum / 10;
+
+if (k < size_r - 1)
+{
+r[k] = (sum % 10) + '0';
+k++;
+}
+else
+{
+return (0);
+}
+
+if (n1[i] != '\0')
+i++;
+if (n2[j] != '\0')
+j++;
+}
+
+r[k] = '\0';
+
+for (i = 0, j = k - 1; i < j; i++, j--)
+{
+char temp = r[i];
+
+r[i] = r[j];
+r[j] = temp;
+}
+
+return (r);
+}
