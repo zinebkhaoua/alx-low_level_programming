18546f488157cbe30c4e4b1bdc1e9632648886be
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-21 06:39:38 -0700

    add 2-strncpy script

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  *_strncpy - copy a string
- *@dest: destination buffer
- *@src: source string
- *@n: maximum number of characters to copy
+ *@dest: destination input
+ *@src: source input
+ *@n: input number
  *Return: pointer to the destination buffer
  */
 char *_strncpy(char *dest, const char *src, int n)
 {
-int j = 0;
+int j
+j = 0;
 
 while (j < n && src[j] != '\0')
 {
