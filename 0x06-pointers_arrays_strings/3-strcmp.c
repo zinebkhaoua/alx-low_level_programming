933252d4e9f95d5259ac0bb6c999c2caefd93646
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-21 07:47:38 -0700

    add 3-strcmp.c script

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  *_strcmp - compares two strings
- *@s1: input value
- *@s2: input value
- *Return: s1[i] - s2[i]
+ *@s1: first input string
+ *@s2: second input string
+ *Return: 0 if s1 and s2 are equal, a negative value if s1 is less than s2,
+ *or a positive value if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-i = 0;
-while (s1[i] != '\0' && s2[i] = '\0')
+int i = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
 {
 if (s1[i] != s2[i])
 {
@@ -17,6 +19,19 @@ return (s1[i] - s2[i]);
 }
 i++;
 }
-return (0);
+/*If we reach here, either one or both strings have reached '\0'.
+ *Compare the lengths to determine the result.
+ */
+if (s1[i] == '\0' && s2[i] == '\0')
+{
+return (0); /* Both strings are equal. */
 }
-
+else if (s1[i] == '\0')
+{
+return (-1); /* s1 is shorter than s2. */
+}
+else
+{
+return (1); /* s1 is longer than s2. */
+}
+}
