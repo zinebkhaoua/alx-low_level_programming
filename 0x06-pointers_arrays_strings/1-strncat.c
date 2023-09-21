373d9618868354e515c469f648ccceb9bb1d9c2d
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-09-21 11:25:34 -0700

    add 1-strncat.c script

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,34 @@
 #include "main.h"
+
 /**
- *_strncat - function that concatenates two strings
- *using at most n bytes from src
- *@dest: input value 
- *@src:input value
- *@n:input value
- *Return: dest
+ * _strncat - Concatenates two strings up to n characters from src.
+ * @dest: The destination string.
+ * @src: The source string to append to dest.
+ * @n: The maximum number of characters to append from src.
+ *
+ * Return: A pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-int j;
-i = 0;
-while (dest[i] != '\0')
+char *ptr = dest;
+
+
+while (*ptr != '\0')
 {
-i++;
+ptr++;
 }
-j = 0;
-while (j < n && src[j] != '\0')
+
+
+while (*src != '\0' && n > 0)
 {
-dest[i] = src[j];
-i++;
-j++;
+*ptr = *src;
+ptr++;
+src++;
+n--;
 }
-dest[i] = '\0';
+
+
+*ptr = '\0';
+
 return (dest);
 }
