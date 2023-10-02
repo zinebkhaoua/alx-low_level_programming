90c3e974659b988ab2cff581f7a7b691d2c8c6dd
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-10-02 14:59:21 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ ** main - entry point
+ **
+ ** Description: Program that adds positive numbers.
+ **
+ ** @argc: number of arguments
+ ** @argv: an array of arguments
+ **
+ ** Return: 0 (successful)
+ **
+ **/
+
+int main(int argc, char **argv)
+{
+int i, res = 0;
+
+for (i = 1; i < argc; i++)
+{
+char *current_arg = argv[i];
+char *ptr = current_arg;
+
+while (*ptr != '\0')
+{
+if (!isdigit(*ptr))
+{
+printf("Error\n");
+return (1);
+}
+ptr++;
+}
+
+res += atoi(current_arg);
+}
+
+printf("%d\n", res);
+
+return (0);
+}
