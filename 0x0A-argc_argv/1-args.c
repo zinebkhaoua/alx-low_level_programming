e636599aae29735dbe96ca39a4adb648f8f33bc6
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-10-02 14:36:54 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ ** main - entry point
+ **
+ ** Description: Program that prints the number of
+ ** arguments passed into it.
+ **
+ ** @argc: number of arguments
+ ** @argv: an array of arguments
+ **
+ ** Return: 0 (successful)
+ **
+ **/
+
+int main(int argc, __attribute__((unused)) char **argv)
+{
+printf("%d\n", argc - 1);
+return (0);
+}
