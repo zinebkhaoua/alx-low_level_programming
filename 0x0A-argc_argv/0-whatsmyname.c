54f7abe263e3608e264025f97b67574638a34283
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-10-02 14:33:03 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ ** main - entry point
+ **
+ ** Description: Program that prints its name, followed by a new line.
+ **
+ ** @argc: number of arguments
+ ** @argv: an array of arguments
+ **
+ ** Return: 0 (successful)
+ **
+ **/
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+char *program_name = argv[0];
+
+printf("%s\n", program_name);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+readme file about 0x0A-argc_argv
