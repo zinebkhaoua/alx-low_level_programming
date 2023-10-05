2c7dbc7924ebb3a7394203d457ef7cd195ef2bef
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-10-05 10:27:14 -0700

    add 4-free_grid.c script

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2-dimensional grid.
+ * @grid: The grid to be freed.
+ * @height: The height of the grid.
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
+
