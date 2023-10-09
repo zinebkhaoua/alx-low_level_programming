801f92dbaf5d0563730aa8b1dff22f605d1a04e0
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-10-09 05:24:09 -0700

    add 0-malloc_checked.c script

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,20 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc and exit if failed
- * @b: int
- * Return: pointer to the array initialized or NULL
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
 {
-int *m = malloc(b);
-if (m == 0)
-exit(98);
-return (m);
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
 }
 
