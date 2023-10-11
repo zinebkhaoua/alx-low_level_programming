60bf47ab21c5886cd6ed1826f0ff24a7e909f809
ZINEB KHAOUA <khaouazineb7@gmail.com>
2023-10-11 10:47:53 -0700

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the name of the source file.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
